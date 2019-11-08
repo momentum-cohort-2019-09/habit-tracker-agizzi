8055f7ecfebd6d2d98e19ea7acef586ac4e4f2e8
agizzi <alexandra.n.gizzi@gmail.com>
2019-11-07 19:05:55 -0500

    add login and register

--- habitproject/urls.py
+++ habitproject/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from goalkeeper import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('goalkeeper.urls')),
+    path('api/', include('goalkeeper.urls')),
+    path('accounts/', include('registration.backends.default.urls')),
+    path('', views.index, name="base"),
 ]
